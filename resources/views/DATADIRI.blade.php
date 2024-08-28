b996762912a1aa37ff77ddc0a4ac2f69d39362d2
InggaChintia <inggainggachintia@gmail.com>
2024-08-28 16:03:41 +0700

    data diri

---
+++ resources/views/DATADIRI.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>KTP INGGA</title>
+</head>
+<body>
+    <h1>--------DATA DIRI MAHASISWA--------</h1>
+        <div class="content">
+        <table>
+            <tr>
+                <td>Nama</td>
+                <td>: INGGA CHINTIA SARI</td>
+            </tr>
+            <tr>
+                <td>NIM</td>
+                <td>: 2241760018</td>
+            </tr>
+            <tr>
+                <td>Jurusan</td>
+                <td>: TEKNOLOGI INFORMASI</td>
+            </tr>
+            <tr>
+                <td>Program Studi</td>
+                <td>: D4 SISTEM INFORMASI BISNIS</td>
+            </tr>
+            <tr>
+                <td>Tempat/Tgl Lahir</td>
+                <td>: LAMONGAN, 25-01-2004</td>
+            </tr>
+            <tr>
+                <td>Jenis Kelamin</td>
+                <td>: PEREMPUAN</td>
+            </tr>
+            <tr>
+                <td>Alamat </td>
+                <td>: KEC.NGIMBANG, KAB.LAMONGAN</td>
+            </tr>
+            <tr>
+                <td>Agama</td>
+                <td>: ISLAM</td>
+            </tr>
+            <tr>
+                <td>Status Perkawinan</td>
+                <td>: BELUM KAWIN</td>
+            </tr>
+            <tr>
+                <td>Pekerjaan</td>
+                <td>: PELAJAR/MAHASISWA</td>
+            </tr>
+            <tr>
+                <td>Kewarganegaraan</td>
+                <td>: WNI</td>
+            </tr>
+        </table>        
+    </div>
+</body>
+</html>
